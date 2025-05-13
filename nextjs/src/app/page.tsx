c4488d0a2beaa9b3c0eef989e8065fb40109c5a6
hoangkhanh31 <hoangkhanhdragon@gmail.com>
2025-05-13 22:05:04 +0700

    init: src FE nextjs

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello Nextjs
+      <div>
+        <Button type="primary">Add new</Button>
+      </div>
+    </div>
+  );
+}
