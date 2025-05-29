18c6681c99e54f5453c28bee8544773c215ec0d5
hoangkhanh31 <hoangkhanhdragon@gmail.com>
2025-05-30 01:07:04 +0700

    feat: API CRUD Users

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const configService = app.get(ConfigService);
-  // const port = configService.get('PORT');
-  await app.listen(process.env.PORT ?? 8080);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,  // chỉ lấy các field đã khai báo trong validation -> các trường khác ngoài validation sẽ bỏ qua
+    forbidNonWhitelisted: true, 
+  }));
+  app.setGlobalPrefix('api/v1', { exclude: [''] }); //set prefix route cho các route 'api/v1' và exclude route '/' không áp dụng prefix
+
+  // await app.listen(process.env.PORT ?? 8080);
+  await app.listen(port ?? 8080);
 }
 bootstrap();
--- nestjs/src/modules/users/dto/create-user.dto.ts
+++ nestjs/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { Prop } from "@nestjs/mongoose";
+import { IsEmail, isEmpty, IsEmpty, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({ message: 'Name không được để trống' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Email không được để trống' })
+    @IsEmail({}, { message: 'Email không đúng định dạng' })
+    email: string;
+
+    @IsNotEmpty({ message: 'Password không được để trống' })
+    password: string;
+
+    @IsEmpty()
+    phone: string;
+
+    @IsEmpty()
+    address: string;
+
+    @IsEmpty()
+    image: string;
+}
